e2ff8c0e318a4b379554d99764768c5d0e71ab99
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-11 19:19:17 +1000

    Refactor collect_ohlcv and get_data functions to accept start and end dates for data collection

--- collection.py
+++ collection.py
@@ -10,32 +10,34 @@ from dotenv import load_dotenv
 import requests
 load_dotenv()
 
-def collect_ohlcv(ticker, period='max', interval='1d'):
+def collect_ohlcv(ticker, start, end):
     """
     Collect OHLCV data for a given ticker using yfinance.
 
     Parameters:
     ticker (str): The stock ticker symbol.
-    period (str): The period for which to collect data (default is 'max').
-    interval (str): The interval between data points (default is '1d').
+    start (str): The start date for the data collection.
+    end (str): The end date for the data collection.
 
     Returns:
     pd.DataFrame: A DataFrame containing the OHLCV data.
     """
-    df = yf.download(ticker, period=period, interval=interval, multi_level_index=False)
+    df = yf.download(ticker, start=start, end=end)
     df.sort_index(ascending=True, inplace=True)
     df.to_csv(f'data/{ticker}_ohlcv.csv')
     print(f"Data collected and saved to: data/{ticker}_ohlcv.csv")
     return df
 
-def get_data(ticker, period='max', interval='1d'):
+
+
+def get_data(ticker, start, end):
     """
     Run all functions in this module to collect various types of data.
 
     Parameters:
     ticker (str): The stock ticker symbol.
-    period (str): The period for which to collect data (default is 'max').
-    interval (str): The interval between data points (default is '1d').
+    start (str): The start date for the data collection.
+    end (str): The end date for the data collection.
 
     Returns:
     None: This function saves dataframes to CSV files in the 'data' directory.
@@ -43,12 +45,14 @@ def get_data(ticker, period='max', interval='1d'):
     Ouptput:
     All data dataframes are saved to the 'data' directory.
     """
-    ohlcv = collect_ohlcv(ticker, period, interval)
+    ohlcv = collect_ohlcv(ticker, start=start, end=end)
     print(ohlcv.head())
     print(ohlcv.info())
 
 
 
 if __name__ == "__main__":
-    ticker = os.getenv('TICKER', '^NSEI')  # Default to Nifty50 if TICKER is not set in .env
-    get_data(ticker, period='max', interval='1d')
+    ticker = os.getenv('TICKER')
+    start = os.getenv('START_DATE')
+    end = os.getenv('END_DATE')
+    get_data(ticker, start, end)
