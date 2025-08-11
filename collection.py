dd46f0280ecc2ab125bdca45afc5fadee6bd7041
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-11 18:19:59 +1000

    Refactor data collection to save OHLCV data directly in collect_ohlcv function

--- collection.py
+++ collection.py
@@ -20,28 +20,12 @@ def collect_ohlcv(ticker, period='max', interval='1d'):
     """
     df = yf.download(ticker, period=period, interval=interval, multi_level_index=False)
     df.sort_index(ascending=True, inplace=True)
-    return df
-
-def get_data(ticker, period='max', interval='1d'):
-    """
-    Get all types of data for a given ticker.
-
-    Parameters:
-    ticker (str): The stock ticker symbol.
-    period (str): The period for which to collect data (default is 'max').
-    interval (str): The interval between data points (default is '1d').
-
-    Returns:
-    pd.DataFrame: A DataFrame containing the data.
-    """
-    # Collect data
-    ohlcv = collect_ohlcv(ticker, period, interval)
-    ohlcv.to_csv(f'data/{ticker}_ohlcv.csv')
+    df.to_csv(f'data/{ticker}_ohlcv.csv')
     print(f"Data collected and saved to: data/{ticker}_ohlcv.csv")
-    return ohlcv
+    return df
 
 if __name__ == "__main__":
     ticker = '^NSEI'
-    data = get_data(ticker, period='max', interval='1d')
+    data = collect_ohlcv(ticker, period='max', interval='1d')
     print(data.head())
     print(data.info())
