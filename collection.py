9c29030fa19e47b2734f12e4c117eb450392a129
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-11 19:06:24 +1000

    Refactor data collection functions to unify data retrieval and improve clarity

--- collection.py
+++ collection.py
@@ -7,6 +7,7 @@ import numpy as np
 import yfinance as yf
 import os
 from dotenv import load_dotenv
+import requests
 load_dotenv()
 
 def collect_ohlcv(ticker, period='max', interval='1d'):
@@ -27,23 +28,27 @@ def collect_ohlcv(ticker, period='max', interval='1d'):
     print(f"Data collected and saved to: data/{ticker}_ohlcv.csv")
     return df
 
-
-def collect_news(start_date=None, end_date=None):
+def get_data(ticker, period='max', interval='1d'):
     """
-    Collect news data from start_date to end_date.
+    Run all functions in this module to collect various types of data.
 
     Parameters:
-    start_date (str): The start date for news collection in 'YYYY-MM-DD' format.
-    end_date (str): The end date for news collection in 'YYYY-MM-DD' format.
-    Returns:
-    pd.DataFrame: A DataFrame containing the news data.
-    """
+    ticker (str): The stock ticker symbol.
+    period (str): The period for which to collect data (default is 'max').
+    interval (str): The interval between data points (default is '1d').
 
+    Returns:
+    None: This function saves dataframes to CSV files in the 'data' directory.
+
+    Ouptput:
+    All data dataframes are saved to the 'data' directory.
+    """
+    ohlcv = collect_ohlcv(ticker, period, interval)
+    print(ohlcv.head())
+    print(ohlcv.info())
 
 
 
 if __name__ == "__main__":
     ticker = os.getenv('TICKER', '^NSEI')  # Default to Nifty50 if TICKER is not set in .env
-    data = collect_ohlcv(ticker, period='max', interval='1d')
-    print(data.head())
-    print(data.info())
+    get_data(ticker, period='max', interval='1d')
