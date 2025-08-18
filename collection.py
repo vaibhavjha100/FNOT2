17dfd6ec7eee6c5e7fe2d702955de1947d49e8bf
Vaibhav Jha <vaibhavjha100@gmail.com>
2025-08-19 00:09:25 +1000

    Add function to collect fundamental data using marketminer

--- collection.py
+++ collection.py
@@ -10,7 +10,7 @@ import os
 from dotenv import load_dotenv
 import sys
 
-from marketminer import scrape_economic_times
+from marketminer import scrape_economic_times, scrape_fundamentals
 import requests
 load_dotenv()
 
@@ -49,6 +49,22 @@ def collect_news(start, end):
     print("News data collected and saved to: data/news.csv")
     return news_df
 
+def collect_fundamentals(ticker):
+    """
+    Collect fundamental data for a given ticker using marketminer.
+
+    Parameters:
+    ticker (str): The stock ticker symbol.
+
+    Returns:
+    dict: A dictionary containing various types of fundamental data.
+    """
+    fund_data = scrape_fundamentals(ticker)
+    for key, value in fund_data.items():
+        value.to_csv(f'data/{ticker}_{key}.csv', index=True)
+        print(f"{key} data saved to: data/{ticker}_{key}.csv")
+    return fund_data
+
 def get_data(ticker, start, end):
     """
     Run all functions in this module to collect various types of data.
@@ -71,6 +87,13 @@ def get_data(ticker, start, end):
     news = collect_news(start=start, end=end)
     print(news.head())
     print(news.info())
+    time.sleep(2)
+    fund = collect_fundamentals(ticker)
+    print(fund.keys())
+    for key, value in fund.items():
+        print(f"{key} DataFrame:")
+        print(value.head())
+        print(value.info())
 
 
 
